829ff141629d120d7dd319e76835eeb1f5005b30
richzjc <zhangjianchuan@wallstreetcn.com>
2017-09-25 16:21:59 +0800

    更新了ReadMe.md文件

--- app/src/main/java/com/android/wscn/appwidget/AppWidget.java
+++ app/src/main/java/com/android/wscn/appwidget/AppWidget.java
@@ -1,26 +1,16 @@
-# AppWidget
-实现一个窗口小组件的功能
+package com.android.wscn.appwidget;
 
-# AppWidget 简介
-App Widget是应用程序窗口小部件（Widget）是微型的应用程序视图，它可以被嵌入到其它应用程序中（比如桌面）并接收周期性的更新。你可以通过一个App Widget Provider来发布一个Widget。
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 
-本文参考[Android官方文本](https://developer.android.com/guide/topics/appwidgets/index.html)，先介绍App Widget的主要组件，然后再以示例来详细说明。
+/**
+ * Created by wscn on 17/9/25.
+ */
 
-# AppWidgetProvider 介绍
-    AppWidgetProvider 是写一个小组件的核心类， 其继承BroadcastReceiver, 具备广播的所有特性。
-
- **重要的几个回调方法**
- * onUpdate
- * onAppWidgetOptionsChanged
- * onDelete
- * onEnable
- * onDisabled
- * onReceive
-
----
-__AppWidgetProvider示例代码__
-
-    public class AppWidget extends AppWidgetProvider {
+public class AppWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -51,4 +41,4 @@ __AppWidgetProvider示例代码__
     public void onEnabled(Context context) {
         super.onEnabled(context);
     }
-    }
+}
